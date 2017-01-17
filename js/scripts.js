8541649cb884c5bb6f2dd917ff438fa09be3302a
Michaela-Davis <michaela.delight@gmail.com>
2017-01-17 13:55:39 -0800

    add displayList feature

--- js/scripts.js
+++ js/scripts.js
@@ -1,20 +1,40 @@
+var shoppingList = [];
 $(document).ready(function(){
-
-  var shoppingList = [];
 
   // On form submit add item to list
   $("form").submit(function(event) {
 
-  // get item from form
-  var item = $("#item").val();
+    // get item from form
+    var item = $("#item").val();
 
-  // add item to shoppingList
-  shoppingList.push(item);
+    // add item to shoppingList
+    shoppingList.push(item);
 
-  // clear input for next item
-  $("#item").clear();
+    // clear input for next item
+    $("#item").val("");
 
-
-    event.preventDefault;
+    event.preventDefault();
   });
+
+  // on button click display list
+  $("#displayList").click(function() {
+
+    // clear list
+    $("#list").empty();
+
+    // map new array with li tags
+    var listToDisplay = shoppingList.map(function(item) {
+      return "<li>" + item + "</li>";
+    });
+
+    // display displayList
+    listToDisplay.forEach(function(item) {
+
+      // use jquery to append to ul
+      $("#list").append(item);
+
+    });
+
+  });
+
 });
